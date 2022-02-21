db3baaa9dd232dda6722b5fee2de85953872d4b6
Edson Alexandre Sensato <edson.sensato@espm.br>
2022-02-21 20:04:34 -0300

    21/02/2022

---
+++ alo-ionic/src/app/contador/contador.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.scss'],
+})
+export class ContadorComponent implements OnInit {
+
+  numero:number = 0;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  executarIncremento() {
+   
+    this.numero++;
+
+  }
+
+}
