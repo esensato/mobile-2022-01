67b9b96add804ce5f0029b641232f5bde06d3b34
Edson Alexandre Sensato <edson.sensato@espm.br>
2022-03-07 22:34:12 -0300

    07/03/2022

--- pizzaria/src/app/pizzaria/pedido.component.ts
+++ pizzaria/src/app/pizzaria/pedido.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+const URL = "https://7c2bad50.us-south.apigw.appdomain.cloud/api/pedido/criar";
 
 @Component({
   selector: 'app-pedido',
@@ -9,8 +12,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class Pedido implements OnInit {
 
   pizzaSelecionada: any;
+  bebidaSelecionada: any;
+  quantidade = 1;
+  total = 0;
+  nome = "";
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
 
@@ -19,12 +26,41 @@ export class Pedido implements OnInit {
       if (this.router.getCurrentNavigation().extras.state) {
 
         this.pizzaSelecionada = this.router.getCurrentNavigation().extras.state.pizzaSelecionada;
-        
+        this.bebidaSelecionada = this.router.getCurrentNavigation().extras.state.bebidaSelecionada;
+
       console.log (this.router.getCurrentNavigation().extras.state.pizzaSelecionada);
       console.log (this.router.getCurrentNavigation().extras.state.bebidaSelecionada);
       }
       });
 
+      this.recalcular(1);
+
 }
+
+  recalcular(valor) {
+    this.quantidade = valor;
+    this.total = this.quantidade * parseFloat(this.bebidaSelecionada.preco) + parseFloat(this.pizzaSelecionada.preco)
+  }
+
+  queijoExtra(valor) {
+   
+    if (valor.checked) {
+    this.total += 10;
+    } else {
+    this.total -= 10;
+    }
+  }
+    
+
+  enviar() {
+
+    let body = {usuario: this.nome, detalhes: {pizza: this.pizzaSelecionada.nome, bebida:
+      this.bebidaSelecionada.nome, total: this.total, quantidade: this.quantidade}}
+
+      this.http.post(URL, body).subscribe((response) => {
+      console.log(response);
+      });
+      
+  }
 
 }
